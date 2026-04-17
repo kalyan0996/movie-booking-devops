c77c36cbefa37f511bbce1a7125a1c42f833d651
JARPULA KALYAN <kalyanjarupla2002@gmail.com>
2026-04-17 17:45:56 +0530

    Initialize App component with MovieList

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MovieList from "./components/MovieList";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{textAlign:"center"}}>Movie Booking</h1>
+      <MovieList />
+    </div>
+  );
+}
+
+export default App;
