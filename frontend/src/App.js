19e0848b93da146695165de422a6a9cf5cbf825e
JARPULA KALYAN <kalyanjarupla2002@gmail.com>
2026-04-17 17:55:59 +0530

    Update App component structure and styles
    Refactor App component layout and add styling.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import MovieList from "./components/MovieList";
+import "./styles.css";
 
 function App() {
   return (
-    <div>
-      <h1 style={{textAlign:"center"}}>Movie Booking</h1>
-      <MovieList />
-    </div>
+    <>
+      <div className="header">Movie Booking</div>
+      <div className="container">
+        <MovieList />
+      </div>
+    </>
   );
 }
 
